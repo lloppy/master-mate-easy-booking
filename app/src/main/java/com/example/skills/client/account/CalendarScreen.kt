718b79b10e05e08dd4d6da5c51abadd3bc762b86
lloppy <ankudinovapol@gmail.com>
2024-05-01 12:18:06 +0500

    add calendar into master account

--- app/src/main/java/com/example/skills/client/account/CalendarScreen.kt
+++ app/src/main/java/com/example/skills/client/account/CalendarScreen.kt
@@ -1,10 +1,64 @@
 package com.example.skills.client.account
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import com.example.skills.master.components.calendar.CalendarView
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CalendarScreen() {
     Text(text = "CalendarScreen")
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.Transparent,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text(
+                        "Настройка графика",
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                        color = Color.Black,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            )
+        },
+    ) { innerPadding ->
+        CustomCalendarView(innerPadding)
+    }
+}
+
+@Composable
+fun CustomCalendarView(
+    innerPadding: PaddingValues,
+) {
+    Column (modifier = Modifier
+        .fillMaxSize()
+        .padding(innerPadding)){
+        CalendarView()
+    }
 }
