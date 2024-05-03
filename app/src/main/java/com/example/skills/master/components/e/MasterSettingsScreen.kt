e800b4c69de2e988a18d9283a1f9023d8f7b7e30
lloppy <ankudinovapol@gmail.com>
2024-05-03 21:08:01 +0500

    change settings design

--- app/src/main/java/com/example/skills/master/components/e/MasterSettingsScreen.kt
+++ app/src/main/java/com/example/skills/master/components/e/MasterSettingsScreen.kt
@@ -61,7 +61,14 @@ fun MasterSettingsScreen(
             )
         },
     ) { innerPadding ->
-        SettingsContent(innerPadding, navigateToEditAccount, navigateToEditPassword, navigateToCalendar, navigateToNotifications, exit)
+        SettingsContent(
+            innerPadding,
+            navigateToEditAccount,
+            navigateToEditPassword,
+            navigateToCalendar,
+            navigateToNotifications,
+            exit
+        )
     }
 }
 
@@ -81,17 +88,39 @@ private fun SettingsContent(
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Column{
-            Spacer(modifier = Modifier.height(16.dp))
-
-            SettingsItem(R.drawable.settings_account, "Данные аккаунта", "Отредактируйте информацию", navigateToEditAccount)
-            SettingsItem(R.drawable.settings_password, "Пароль", "Измените свой пароль", navigateToEditPassword)
-            SettingsItem(R.drawable.settings_calendar, "Google Календарь", "Синхронизируйте расписание", navigateToCalendar)
-            SettingsItem(R.drawable.settings_notification, "Уведомления", "Управляйте своими уведомлениями", navigateToNotifications)
+        Column {
+            SettingsItem(
+                R.drawable.settings_account,
+                "Данные аккаунта",
+                "Отредактируйте информацию",
+                navigateToEditAccount
+            )
+            SettingsItem(
+                R.drawable.settings_password,
+                "Пароль",
+                "Измените свой пароль",
+                navigateToEditPassword
+            )
+            SettingsItem(
+                R.drawable.settings_calendar,
+                "Google Календарь",
+                "Синхронизируйте расписание",
+                navigateToCalendar
+            )
+            SettingsItem(
+                R.drawable.settings_notification,
+                "Уведомления",
+                "Управляйте своими уведомлениями",
+                navigateToNotifications
+            )
         }
-        
-        TextButton(onClick = exit , modifier = Modifier.padding(bottom = 90.dp)) {
-            Text(text = "Выйти", color = Color.Red, fontSize = 14.sp, fontWeight = FontWeight.SemiBold)
+        TextButton(onClick = exit, modifier = Modifier.padding(bottom = 90.dp)) {
+            Text(
+                text = "Выйти",
+                color = Color.Red,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.SemiBold
+            )
         }
     }
 }
@@ -101,7 +130,7 @@ fun SettingsItem(icon: Int, title: String, desc: String, navigateTo: () -> Unit)
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .height(100.dp)
+            .height(88.dp)
             .padding(start = 16.dp, end = 16.dp),
         horizontalAlignment = Alignment.Start,
         verticalArrangement = Arrangement.Center
@@ -111,18 +140,17 @@ fun SettingsItem(icon: Int, title: String, desc: String, navigateTo: () -> Unit)
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Row (
+            Row(
                 horizontalArrangement = Arrangement.Start,
                 verticalAlignment = Alignment.CenterVertically
-            )  {
+            ) {
                 Icon(
                     painter = painterResource(id = icon),
                     contentDescription = "icon",
-                    modifier = Modifier.height(60.dp)
+                    modifier = Modifier.height(52.dp)
                 )
-
-                Column (modifier = Modifier.padding(start = 12.dp)) {
-                    Text(text = title, fontSize = 14.sp, fontWeight = FontWeight.Medium)
+                Column(modifier = Modifier.padding(start = 12.dp)) {
+                    Text(text = title, fontSize = 16.sp, fontWeight = FontWeight.Medium)
                     Spacer(modifier = Modifier.height(6.dp))
                     Text(
                         text = desc,
