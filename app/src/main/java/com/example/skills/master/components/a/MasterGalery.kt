bf20d027fc7a0f010a4aae461630b72aad37a61d
lloppy <ankudinovapol@gmail.com>
2024-05-04 19:42:23 +0500

    refactor first version of master personal account

--- app/src/main/java/com/example/skills/master/components/a/MasterGalery.kt
+++ app/src/main/java/com/example/skills/master/components/a/MasterGalery.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -16,18 +15,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.rememberImagePainter
 import com.example.skills.R
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun MasterGallery(images: List<Uri>) {
     LazyVerticalGrid(
-        modifier = Modifier.fillMaxWidth().padding(bottom = 90.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 90.dp),
         columns = GridCells.Fixed(3),
-        contentPadding = PaddingValues(2.dp),
+        contentPadding = PaddingValues(6.dp),
         content = {
             items(images.size) { index ->
                 Image(
@@ -41,27 +39,5 @@ fun MasterGallery(images: List<Uri>) {
                 )
             }
         }
-    )
-}
-
-
-@Preview
-@Composable
-fun prew(){
-    MasterGallery(
-         listOf(
-            Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    Uri.parse("https://img.freepik.com/free-vector/aesthetic-background-vector-dried-flower-with-shadow-glitter-design_53876-157555.jpg"),
-    )
     )
 }
--- app/src/main/java/com/example/skills/master/components/a/MasterHomeScreen.kt
+++ app/src/main/java/com/example/skills/master/components/a/MasterHomeScreen.kt
@@ -2,7 +2,9 @@ package com.example.skills.master.components.a
 
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -13,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material.icons.outlined.Add
@@ -43,6 +47,7 @@ import androidx.navigation.NavHostController
 import com.example.skills.R
 import com.example.skills.master.components.b.calendar.clickable
 import com.example.skills.master.components.d.paddingBetweenText
+import com.example.skills.master.navigation.account.ScreenMaster
 import com.example.skills.repository.MainViewModel
 import com.example.skills.ui.theme.fontFamilyInter
 
@@ -71,7 +76,9 @@ fun MainMasterScreen(
                     )
                 },
                 actions = {
-                    IconButton(onClick = { /* do something */ }) {
+                    IconButton(onClick = {
+                        val profileId = master.id
+                    }) {
                         Icon(
                             imageVector = Icons.Filled.Share,
                             contentDescription = "Localized description"
@@ -104,6 +111,7 @@ fun MasterHomeScreen(
 ) {
     val mainViewModel = MainViewModel()
     val master = mainViewModel.getMaster()
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -111,109 +119,7 @@ fun MasterHomeScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.22f)
-                .padding(bottom = 12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
-        ) {
-            val context = LocalContext.current
-            val imageName = master.imageId ?: "master"
-            val imageId =
-                context.resources.getIdentifier(imageName, "drawable", context.packageName)
-            val painter =
-                if (imageId != 0) painterResource(id = imageId) else painterResource(id = R.drawable.master)
-
-            Image(
-                painter = painter,
-                contentDescription = "Master image",
-                Modifier.fillMaxSize(),
-                alignment = Alignment.Center
-            )
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(start = 8.dp, end = 8.dp, bottom = 6.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
-        ) {
-            var expanded by remember { mutableStateOf(false) }
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = master.description,
-                    fontSize = 14.sp,
-                    maxLines = if (expanded) Int.MAX_VALUE else 3,
-                    fontFamily = fontFamilyInter,
-                    fontWeight = FontWeight.Normal,
-                    textAlign = TextAlign.Center,
-//                lineHeight = 18.sp,
-                    color = Color.Gray,
-                    modifier = Modifier.padding(start = 12.dp, end = 12.dp)
-                )
-                if (!expanded) {
-                    Text(
-                        text = "... ещё",
-                        color = Color.Gray,
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Normal,
-                        modifier = Modifier
-                            .clickable {
-                                expanded = !expanded
-                            })
-                }
-
-            }
-            if (expanded) {
-                Spacer(modifier = Modifier.height(paddingBetweenText))
-                Text(
-                    text = master.address,
-                    fontSize = 14.sp,
-                    fontFamily = fontFamilyInter,
-                    fontWeight = FontWeight.Normal,
-                    textAlign = TextAlign.Center,
-//                lineHeight = 18.sp,
-                    color = Color.Black,
-                    modifier = Modifier.padding(start = 8.dp, end = 8.dp)
-                )
-            }
-            Spacer(modifier = Modifier.height(paddingBetweenText))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_link),
-                    contentDescription = "link",
-                    tint = Color(0, 122, 255),
-                )
-                val context = LocalContext.current
-                val uri = Uri.parse(master.linkCode)
-                val intent = Intent(Intent.ACTION_VIEW, uri)
-
-                Text(
-                    text = master.linkCode,
-                    fontSize = 14.sp,
-                    fontFamily = fontFamilyInter,
-                    fontWeight = FontWeight.Normal,
-                    textAlign = TextAlign.Center,
-//                lineHeight = 18.sp,
-                    color = Color(0, 122, 255),
-                    modifier = Modifier
-                        .padding(start = 8.dp)
-                        .clickable {
-                            context.startActivity(intent)
-                        }
-                )
-                Spacer(modifier = Modifier.height(paddingBetweenText))
-            }
-            MasterGallery(master.images)
-        }
+        ProfileHead(master)
+        MasterGallery(master.images)
     }
 }
